3cbcdc79f34c21f1559a97b360e791252ef8dc51
ygd <yasin.guezeldal@invision.de>
2017-12-13 09:38:11 +0100

    Update deps and bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExKonsument.Mixfile do
 
   def project do
     [app: :exkonsument,
-     version: "3.1.2",
+     version: "3.1.3",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
