921451f3f7c643fb681468faef6b1386bcc77a9e
David Zok <david.zok@invision.de>
2018-11-14 09:59:55 +0100

    Update Elixir and Erlang

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExKonsument.Mixfile do
     [
       app: :exkonsument,
       version: "3.2.3",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
