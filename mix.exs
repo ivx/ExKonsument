ea1d5d54b1e6b773455425c1535a2b4c66e71728
ygd <yasin.guezeldal@invision.de>
2019-01-11 10:37:51 +0100

    Bump version to 3.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKonsument.Mixfile do
   def project do
     [
       app: :exkonsument,
-      version: "3.3.0",
+      version: "3.3.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
