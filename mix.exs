15dd3ecce06c7ffbd002d4a905957f4c768a4412
Mario Mainz <mario.mainz@invision.de>
2019-04-26 10:46:22 +0200

    Bump version to 4.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKonsument.Mixfile do
   def project do
     [
       app: :exkonsument,
-      version: "4.0.0",
+      version: "4.0.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
