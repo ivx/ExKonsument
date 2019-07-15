fb52cb13b75f7c00676179ea6347460f1a1a64a9
Mario Mainz <mario.mainz@invision.de>
2019-07-15 13:12:25 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule ExKonsument.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:amqp, "~> 1.1"},
+      {:amqp, "~> 1.2.2"},
       {:jason, "~> 1.0"},
       {:connection, "~> 1.0"},
       {:credo, "~> 1.0", only: [:dev, :test]},
