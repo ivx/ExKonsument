bcfbedb35db5430f93b5a67aa3aa877b1829cd12
Thomas Fruetel <github@fruetel.de>
2021-02-22 14:50:15 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExKonsument.Mixfile do
   def project do
     [
       app: :exkonsument,
-      version: "4.1.2",
+      version: "4.2.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
